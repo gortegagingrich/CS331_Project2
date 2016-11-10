52c02d5c03a4205eb8c4902349b53cdf70a5b831
Gabriel <gortegagingrich@gmail.com>
2016-11-10 09:39:04 -0800

    mergesort is implemented and it now handles larger lists before segfaulting from overflows

--- Project2.c
+++ Project2.c
@@ -2,23 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-void quicksort(int*, int, int);
-void swap(int*, int*);
-void printArr(int*, int);
-void fillArr(int*, int);
+void mergesort(short*,int,int);
+void merge(short*,short*,int,short*,int);
+void swap(short*, short*);
+void printArr(short*, int);
+void fillArr(short*, int);
 void calcTime(clock_t, clock_t);
+
+clock_t seed;
 
 // Project specific algorithms
 
-/* Algorithm 1 works by sorting the given array (using quicksort)
+/* Algorithm 1 works by sorting the given array (using merge sort)
  * and returning the kth term.
  */
-int algorithm1 (int* arr, int length, int k) {
+int algorithm1 (short* arr, int length, int k) {
 	clock_t start, end;
 	int out;
 
 	start = clock();
-	quicksort(arr,0,length-1);
+	mergesort(arr,0,length-1);
 	out = arr[k];
 	calcTime(start,clock());
 
@@ -28,7 +31,7 @@ int algorithm1 (int* arr, int length, int k) {
 /* Algorithm 2 works by using the partition method from quicksort
  * until the pivot is equal to k and returning the kth term.
  */
-int algorithm2(int* arr, int length, int k) {
+int algorithm2(short* arr, int length, int k) {
 	clock_t start;
 	int a, b, pivot, out;
 
@@ -58,7 +61,7 @@ int algorithm2(int* arr, int length, int k) {
  * The only difference is that before it calls partition,
  * it swaps the middle and right indeces.
  */
-int algorithm3(int* arr, int length, int k) {
+int algorithm3(short* arr, int length, int k) {
 	int pivot, out, a, b;
 	clock_t start;
 
@@ -67,7 +70,6 @@ int algorithm3(int* arr, int length, int k) {
 	b = length - 1;
 
 	for (;;) {
-
 		swap(&arr[(a + b) / 2], &arr[b]);
 
 		pivot = partition(arr,a,b);
@@ -88,18 +90,48 @@ int algorithm3(int* arr, int length, int k) {
 }
 
 // Array functions
-void quicksort(int* arr, int p, int r) {
-	int q;
+void mergesort(short* arr, int a, int b) {
+	int mid, i;
+	short *left, *right;
 
-	if (p < r) {
-		q = partition(arr,p,r);
+	if ((b-a) >= 2) {
+		mid = (a + b) / 2;
 
-		quicksort(arr,p,q-1);
-		quicksort(arr,q+1,r);
+		left = (short*)malloc((mid-a)*sizeof(short));
+		right = (short*)malloc((b-mid)*sizeof(short));
+
+		merge(arr,left,mid-a,right,b-mid);
+
+		free(left);
+		free(right);
 	}
 }
 
-int partition(int* arr, int p, int r) {
+void merge(short* arr, short* left, int leftLength, short*right, int rightLength) {
+	int i,j,k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+
+	while (i < leftLength && j < rightLength) {
+		if (left[i] < right[j]) {
+			arr[k++] = left[i++];
+		} else {
+			arr[k++] = right[j++];
+		}
+	}
+
+	for (;i < leftLength; i++) {
+		arr[k++] = left[i];
+	}
+
+	for (;j < rightLength; j++) {
+		arr[k++] = right[j];
+	}
+}
+
+int partition(short* arr, int p, int r) {
 	int x, i, j;
 
 	x = arr[r];
@@ -118,14 +150,14 @@ int partition(int* arr, int p, int r) {
 	return i + 1;
 }
 
-void swap(int* a, int* b) {
+void swap(short* a, short* b) {
 	int temp;
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-void printArr(int* list, int length) {
+void printArr(short* list, int length) {
 	int i;
 
 	for (i = 0; i < length; i++) {
@@ -134,7 +166,7 @@ void printArr(int* list, int length) {
 	printf("\n");
 }
 
-void fillArr(int* list, int length) {
+void fillArr(short* list, int length) {
 	int i;
 
 	for (i = 0; i < length; i++) {
@@ -147,48 +179,52 @@ void calcTime(clock_t start, clock_t end) {
 	printf("%f\n", (float)(end - start) / 1000000.0 );
 }
 
+void performTest(int algNumber, short* list, uint n, uint k) {
+	srand(seed);
+	fillArr(list,n);
+	printf("\t\tAlgorithm %d: ", algNumber);
+
+	switch (algNumber) {
+		case 1:
+			algorithm1(list, n, k*(n-1)/4);
+			break;
+		case 2:
+			algorithm2(list, n, k*(n-1)/4);
+			break;
+		case 3:
+			algorithm3(list, n, k*(n-1)/4);
+			break;
+	}
+}
+
 // main function
 int main(int argc, char **argv) {
-	int i, j, numTimes, maxN, k;
-	clock_t seed;
+	// i and maxN need to be unsigned to prevent segfaults caused by overflows
+	uint i, maxN;
+	int j, numTimes, k, l;
 
 	numTimes = atoi(argv[1]);
 	maxN = atoi(argv[2]);
+	seed = clock();
 
 	if (argc == 3) {
 		for (i = 10; i <= maxN; i *= 10) {
 			printf("N: %d\n", i);
 
-			for (k = 1; k <= 4; k++) {
-				printf("K: %.2f\n", (k / 4.));
+			short* list;
+			list = malloc(sizeof(short) * i);
 
-				for (j = 0; j < numTimes; j++) {
-					int* list;
-					list = malloc(sizeof(int) * i);
-					seed = clock();
+			for (j = 0; j < numTimes; j++) {
+				for (k = 0; k <= 4; k++) {
+					printf("\tK: %d\n", (k == 0 ? 1 : (i * k / 4)));
 
-					srand(seed);
-					fillArr(list,i);
-					printf("Algorithm1: ");
-					algorithm1(list,i, k*(i-1)/4);
-					//printf("n: %d\nK: %d\nKth: %d\n", i, i/4, algorithm1(list,i, i/4));
-
-					srand(seed);
-					fillArr(list,i);
-					printf("Algorithm2: ");
-					algorithm2(list,i, k*(i-1)/4);
-					//printf("n: %d\nK: %d\nKth: %d\n", i, i/4, algorithm2(list,i, i/4));
-
-					srand(seed);
-					fillArr(list,i);
-					printf("Algorithm3: ");
-					algorithm3(list,i, k*(i-1)/4);
-					//printf("n: %d\nK: %d\nKth: %d\n\n", i, i/4, algorithm3(list,i, i/4));
-
-					free(list);
+					for (l = 1; l <= 3; l++) {
+						performTest(l,list,i,k);
+					}
 				}
 			}
 
+			free(list);
 			printf("\n");
 		}
 	}
